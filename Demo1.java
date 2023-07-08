549e77d71820cb4d421f8f1391158b2d657e91fd
Prashant08Pal <prashantpalofficial04gmail.com>
2023-07-08 22:51:49 +0530

    wrapper

---
+++ Demo1.java
@@ -0,0 +1,13 @@
+package wrapper;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		int a=20;
+		Integer i =Integer.valueOf(a);
+		Integer j=i;
+		System.out.println(a+" "+i+" "+j);
+		
+		
+		
+	}	
+}
---
+++ Demo2.java
@@ -0,0 +1,13 @@
+package wrapper;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+ Integer a=new Integer(3);
+ int i=a.intValue();
+ int j=i;
+	System.out.println(a+" "+i+" "+j);
+
+	}
+
+}
